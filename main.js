f6b8c5c06583b62d99ec1df21ce29bf5a8996a9f
Jaren Mitchell <jaren_mitchell@yahoo.com>
2017-06-12 02:44:54 -0500

    first commit

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
 var container = document.querySelector(".container");
 
+//console.log(formData[5]);
 
-
-
-
-for (i = 0; i < formData.length; i++) {
+console.log(formData[5])
+for (var i = 0; i < formData.length; i++) {
     
     var control = formData [i];
 
     if (control.type == "select"){
+        console.log(control.type)
         var inputElement = document.createElement('select');
-        console.log(control.options.length);
+        //console.log(control.options.length);
+        
         var options = control.options
-        for (i =0; i < options.length; i++){
+       
+        for (var n = 0; n < options.length; n++){
             var optionElement = document.createElement("option");
-            optionElement.value = options[i].value;
-            optionElement.innerHTML = options[i].label;
+            optionElement.value = options[n].value;
+            optionElement.innerHTML = options[n].label;
             inputElement.appendChild(optionElement);
         }
 
     }
-    
+
     else if (control.type == "textarea"){
+        console.log("Hi!");
         var inputElement = document.createElement('textarea');
     }
     
@@ -29,11 +32,9 @@ for (i = 0; i < formData.length; i++) {
         var inputElement = document.createElement('input');
     }
 
-    
     inputElement.id = control.id;
     inputElement.type = control.type;
-    inputElement.placeholder = control.label
-
+    inputElement.placeholder = control.label;
 
     container.appendChild(inputElement);
 
